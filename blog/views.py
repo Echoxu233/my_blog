8d959945d35f3a53f8e4e8963aab5e56bd370b99
767293803@qq.com <767292803@qq.com>
2018-04-13 15:22:24 +0800

    2

---
+++ blog/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .models import Comment
+def home_page(request):
+
+    if request.method == 'POST':
+        comment = request.POST.get('comment')
+        user = request.POST.get("username")
+
+        comment_obj = Comment.objects.create(
+        	Comment=comment,
+        	user=user
+        )
+
+
+
+
+        print(comment * 10)
+        return render(request, 'blog/home_page.html', {'comment':comment_obj})
+
+    return render(request, 'blog/home_page.html', {'test': 'Hey this is a test can you see this?'} )
+    
+# Create your views here.
